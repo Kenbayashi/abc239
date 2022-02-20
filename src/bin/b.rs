d55dbfd08a98d645e8158c9045910105699cbb26
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-21 00:34:42 +0900

    b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,3 +1,12 @@
 fn main() {
-    unimplemented!();
+    let mut input = String::new();
+
+    std::io::stdin().read_line(&mut input)
+                    .expect("input failed");
+
+    let num = input.trim()
+                   .parse::<i64>()
+                   .unwrap();
+
+    println!("{}", num.div_euclid(10));
 }
