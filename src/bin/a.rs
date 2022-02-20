b14255a3821ce61c6dc11cc4aaaf95585cb2155f
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-20 23:34:53 +0900

    a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::io::Read;
+use std::str::FromStr;
+
 fn main() {
-    unimplemented!();
+    let x = read::<f64>();
+
+    println!("{}", (x * (12800000f64 + x)).sqrt());
+}
+
+fn read<T: FromStr>() -> T {
+    io::stdin().bytes()
+               .map(|c| c.unwrap() as char)
+               .skip_while(|c| c.is_whitespace())
+               .take_while(|c| !c.is_whitespace())
+               .collect::<String>()
+               .parse::<T>()
+               .ok()
+               .unwrap()
 }
